20edd664276cf13dcdaae35186dbca8b804f67ff
Sérgio <75281582+sergiogbernardo@users.noreply.github.com>
2021-07-31 19:58:09 -0300

    Add files via upload

---
+++ projects/Curso Qoda/notdevs.py
@@ -0,0 +1,17 @@
+
+#print e input
+print("Hello word")
+
+#Armezana dentro de x
+x = input("Qual o seu nome ?")
+print(f"Ola, {x}")
+
+#Tipos de dados
+#String
+string = "Oi sou uma instrig"
+print(type(string))
+
+#Inteiro(int)
+y = 3
+print(type(y))
+
